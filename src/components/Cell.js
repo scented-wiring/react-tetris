5d7c01f0c8e2983f7d1a29df2a6aaeee968bce24
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-03-04 10:52:04 +0000

    style components

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,3 +1,8 @@
-const Cell = ({ type }) => <div>Cell</div>;
+import { StyledCell } from "./styles/StyledCell";
+import { TETROMINOS } from "../terominos";
+
+const Cell = ({ type }) => (
+  <StyledCell type={type} color={TETROMINOS[type].color} />
+);
 
 export default Cell;
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,3 +1,7 @@
-const Display = ({ gameOver, text }) => <div>{text}</div>;
+import { StyledDisplay } from "./styles/StyledDisplay";
+
+const Display = ({ gameOver, text }) => (
+  <StyledDisplay gameOver={gameOver}>{text}</StyledDisplay>
+);
 
 export default Display;
