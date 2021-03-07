98f15c00efbbcd61740d7cad70c9d24bd965038c
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-03-07 16:11:41 +0000

    add rotation

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -9,15 +9,15 @@ export const createStage = () =>
 export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
   for (let y = 0; y < player.tetromino.length; y += 1) {
     for (let x = 0; x < player.tetromino[y].length; x += 1) {
-      // 1. Check that we're on a tetromino cell
+      // 1. Check that we're on an actual Tetromino cell
       if (player.tetromino[y][x] !== 0) {
         if (
-          // 2. Check that our move is inside the game area's height (y)
-          // We shouldn't go through the bottom of the play area
+          // 2. Check that our move is inside the game areas height (y)
+          // That we're not go through bottom of the play area
           !stage[y + player.pos.y + moveY] ||
-          // 3. Check that our move is inide the game area's width (x)
+          // 3. Check that our move is inside the game areas width (x)
           !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
-          // 4. Check cell we're moving to isn't set to clear
+          // 4. Check that the cell wer'e moving to isn't set to clear
           stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
             "clear"
         ) {
